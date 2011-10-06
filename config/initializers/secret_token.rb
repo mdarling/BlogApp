cd11c4a1188878d6751451c2db6bb1acd90c2fda
Michael Darling <mdarling@ece.unm.edu>
2011-10-06 09:52:01 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b19711d3f4b29bae9fb021d3c6b798856ba83f2b3b2eb3a524f3dacfd75620a6f2399963948f7eea36cefddb0aeeee0f0c79d3b6aa46cee2dd8acda821064b80'
